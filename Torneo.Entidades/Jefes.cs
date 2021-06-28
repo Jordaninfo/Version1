97e80d71447781c02fa0d434e994f8b86ab2e9ac
jordaninfo <jec.ortega@yavirac.edu.ec>
2021-06-28 15:04:57 -0500

    Hola mundo! c#

---
+++ Torneo.Entidades/Jefes.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Torneo.Entidades
+{
+    public class Jefes : Personaje
+    {
+        public string Habilidad { get; set; } 
+    }
+}
+
---
+++ Torneo.Entidades/Jugador.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Torneo.Entidades
+{
+    public class Jugador : Personaje
+    {
+        public string Apellido { get; set;}
+    }
+}
+
